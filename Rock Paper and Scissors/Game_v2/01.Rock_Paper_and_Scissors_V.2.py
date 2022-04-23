a5f68233e13287fba2760e19156a218c1db4f3df
Marcos Henrique Silva Quirino <marcoshsq@gmail.com>
2022-04-23 02:25:29 -0300

    Update 01.Rock_Paper_and_Scissors_V.2.py

--- Rock Paper and Scissors/Game_v2/01.Rock_Paper_and_Scissors_V.2.py
+++ Rock Paper and Scissors/Game_v2/01.Rock_Paper_and_Scissors_V.2.py
@@ -9,8 +9,20 @@ game_window.title("Rock, Paper & Scissors")
 game_window.configure(background="#5533cc")
 
 # Icons
+paper_left = ImageTk.PhotoImage(Image.open("paper_left.png"))
+paper_right = ImageTk.PhotoImage(Image.open("paper_right.png"))
 
+rock_left = ImageTk.PhotoImage(Image.open("rock_left.png"))
+rock_right = ImageTk.PhotoImage(Image.open("rock_right.png"))
 
+scissors_left = ImageTk.PhotoImage(Image.open("scissors_left.png"))
+scissors_right = ImageTk.PhotoImage(Image.open("scissors_right.png"))
+
+# Inserting the icons.
+player_label = Label(game_window, image=scissors_left.png)
+python_label = Label(game_window, image=scissors_right.png)
+player_label.grid(row=1, column=0)
+python_label.grid(row=1, column=4)
 
 # Main Loop.
 game_window.mainloop()
