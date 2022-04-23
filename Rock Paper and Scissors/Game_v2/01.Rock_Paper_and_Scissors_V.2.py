117a2d26d39b15c49159a8a8fe3c930e76444455
Marcos Henrique Silva Quirino <marcoshsq@gmail.com>
2022-04-23 02:11:49 -0300

    update

--- Rock Paper and Scissors/Game_v2/01.Rock_Paper_and_Scissors_V.2.py
+++ Rock Paper and Scissors/Game_v2/01.Rock_Paper_and_Scissors_V.2.py
@@ -1,9 +1,15 @@
 from tkinter import *
+from PIL import Image, ImageTk
 
 # Main Window.
 game_window = Tk()
 # Window name.
 game_window.title("Rock, Paper & Scissors")
+# BG collor.
+game_window.configure(background="#5533cc")
+
+# Icons
+
 
 
 # Main Loop.
